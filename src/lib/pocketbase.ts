598199a145ecff56708a8806bde49963b1efbc80
Pascal Burri <mail@pascal-burri.com>
2025-06-02 14:27:48 +0200

    fix pocketbase url

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,10 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase(process.env.PB_URL!);
+export const pb = new PocketBase(
+	process.env.PB_URL ||
+		process.env.NEXT_PUBLIC_PB_URL ||
+		'http://localhost:8090',
+);
 
 // Persist auth store across server/client by using cookies:
 // In server components this reads cookies automatically; in client you’ll sync manually.
