2a53b237ec4ae1c53bd1323039ecd9cb7adf05d6
Pascal Burri <mail@pascal-burri.com>
2025-06-02 09:09:00 +0200

    add mobile header, refractor a bit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import {Toaster} from 'sonner';
 import './globals.css';
 import type {Metadata} from 'next';
-import Header from './components/Header';
 
 export const metadata: Metadata = {
 	title: 'StudyBuddy',
@@ -9,32 +8,10 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-	const navItems = [
-		{
-			name: 'home',
-			href: '/',
-		},
-		{
-			name: 'Study Groups',
-			href: '/studygroups',
-		},
-		{
-			name: 'Calendar',
-			href: '/calendar',
-		},
-		{
-			name: 'Profile',
-			href: '/profile',
-		},
-	];
-
 	return (
 		<html lang="en">
 			<body className="bg-background text-foreground selection:bg-primary/20 flex min-h-full flex-col scroll-smooth antialiased">
-				<Header navItems={navItems} />
-				<main className="min-h-view-full flex w-full flex-col items-center justify-center pt-24">
-					{children}
-				</main>
+				{children}
 				<Toaster />
 			</body>
 		</html>
