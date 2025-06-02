7396e62686884134647ea783215f81fa421c58ce
e550036 <timo.balsiger@sbb.ch>
2025-06-02 09:35:11 +0200

    Fix Profile

--- src/components/editableProfile.tsx
+++ src/components/editableProfile.tsx
@@ -25,7 +25,7 @@ export default function EditableProfile({ user }: { user: any }) {
       <div className="flex items-center space-x-4">
         <Avatar className="w-16 h-16 rounded-full">
           <AvatarImage
-            src={`http://localhost:8090/api/files/_pb_users_auth_/${profile.id}/${profile.avatar}`}
+            src={`${process.env.PB_URL}/${process.env.NEXT_PUBLIC_PB_URL}/${profile.id}/${profile.avatar}`}
             alt={profile.name}
           />
           <AvatarFallback>{profile.name?.charAt(0)}</AvatarFallback>
